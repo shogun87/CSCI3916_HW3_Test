211b4845934d1ef0b1eba1a834b2e5b620926d5c
shogun87 <45832125+shogun87@users.noreply.github.com>
2022-03-09 12:53:00 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,92 @@
+/*
+CSC3916 HW2
+File: Server.js
+Description: Web API scaffolding for Movie API
+ */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var authController = require('./auth');
+var authJwtController = require('./auth_jwt');
+var jwt = require('jsonwebtoken');
+var cors = require('cors');
+var User = require('./Users');
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+var router = express.Router();
+
+function getJSONObjectForMovieRequirement(req) {
+    var json = {
+        headers: "No headers",
+        key: process.env.UNIQUE_KEY,
+        body: "No body"
+    };
+
+    if (req.body != null) {
+        json.body = req.body;
+    }
+
+    if (req.headers != null) {
+        json.headers = req.headers;
+    }
+
+    return json;
+}
+
+router.post('/signup', function(req, res) {
+    if (!req.body.username || !req.body.password) {
+        res.json({success: false, msg: 'Please include both username and password to signup.'})
+    } else {
+        var user = new User();
+        user.name = req.body.name;
+        user.username = req.body.username;
+        user.password = req.body.password;
+
+        user.save(function(err){
+            if (err) {
+                if (err.code == 11000)
+                    return res.json({ success: false, message: 'A user with that username already exists.'});
+                else
+                    return res.json(err);
+            }
+
+            res.json({success: true, msg: 'Successfully created new user.'})
+        });
+    }
+});
+
+router.post('/signin', function (req, res) {
+    var userNew = new User();
+    userNew.username = req.body.username;
+    userNew.password = req.body.password;
+
+    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
+        if (err) {
+            res.send(err);
+        }
+
+        user.comparePassword(userNew.password, function(isMatch) {
+            if (isMatch) {
+                var userToken = { id: user.id, username: user.username };
+                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                res.json ({success: true, token: 'JWT ' + token});
+            }
+            else {
+                res.status(401).send({success: false, msg: 'Authentication failed.'});
+            }
+        })
+    })
+});
+
+app.use('/', router);
+app.listen(process.env.PORT || 8080);
+module.exports = app; // for testing only
+
+
---
+++ test/testcollection.js
@@ -0,0 +1,57 @@
+let envPath = __dirname + "/../.env"
+require('dotenv').config({path:envPath});
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let User = require('../Users');
+chai.should();
+
+chai.use(chaiHttp);
+
+let login_details = {
+    name: 'test',
+    username: 'email@email.com',
+    password: '123@abc'
+}
+
+describe('Register, Login and Call Test Collection with Basic Auth and JWT Auth', () => {
+   beforeEach((done) => { //Before each test initialize the database to empty
+       //db.userList = [];
+
+       done();
+    })
+
+    after((done) => { //after this test suite empty the database
+        //db.userList = [];
+        User.deleteOne({ name: 'test'}, function(err, user) {
+            if (err) throw err;
+        });
+        done();
+    })
+
+    //Test the GET route
+    describe('/signup', () => {
+        it('it should register, login and check our token', (done) => {
+          chai.request(server)
+              .post('/signup')
+              .send(login_details)
+              .end((err, res) =>{
+                console.log(JSON.stringify(res.body));
+                res.should.have.status(200);
+                res.body.success.should.be.eql(true);
+                //follow-up to get the JWT token
+                chai.request(server)
+                    .post('/signin')
+                    .send(login_details)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.have.property('token');
+                        let token = res.body.token;
+                        console.log(token);
+                        done();
+                    })
+              })
+        })
+    });
+
+});
